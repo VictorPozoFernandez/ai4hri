b698f191a3c49d8846988d099620eaf84f9eaee5
Pousen <victorpozo125@gmail.com>
2023-02-16 16:45:04 +0100

    Implement toghether whisper and ada node

--- scripts/Ada_node.py
+++ scripts/Ada_node.py
@@ -1,23 +1,46 @@
+import rospy
+from std_msgs.msg import String
 import openai
 import os
 import numpy as np
 from sklearn.cluster import KMeans
 
-openai.organization = os.environ.get("OPENAI_ORG_ID")
-openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-model = "text-embedding-ada-002"
+def main():
 
-res = openai.Embedding.create( input = ["this camera is available in black, white and silver"], engine=model)
+    rospy.init_node("Ada", anonymous=True)
+    rospy.loginfo("Node Ada initialized. Listening...")
+    rospy.Subscriber("/ai4hri/utterance", String, callback)
 
-list=[]
-for vec in res["data"]:
-    list.append(vec["embedding"])
+    rospy.spin()
 
-arr = np.asarray(list)
-keyword_dict = np.load('/home/victor/catkin_ws_AI4HRI/src/ai4hri/scripts/keyword_dict.npy',allow_pickle='TRUE').item()
-kmeans = np.load('/home/victor/catkin_ws_AI4HRI/src/ai4hri/scripts/kmeans_model.npy',allow_pickle='TRUE').item()
+def callback(msg):
 
-y = kmeans.predict(arr)
-print("Cluster:" + str(y[0]))
-print("Keywords:" + str(keyword_dict[y[0]]))
+        openai.organization = os.environ.get("OPENAI_ORG_ID")
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+        model = "text-embedding-ada-002"
+
+        res = openai.Embedding.create( input = [msg.data], engine=model)
+
+        list=[]
+        for vec in res["data"]:
+            list.append(vec["embedding"])
+
+        arr = np.asarray(list)
+        keyword_dict = np.load('/home/victor/catkin_ws_AI4HRI/src/ai4hri/scripts/keyword_dict.npy',allow_pickle='TRUE').item()
+        kmeans = np.load('/home/victor/catkin_ws_AI4HRI/src/ai4hri/scripts/kmeans_model.npy',allow_pickle='TRUE').item()
+
+        y = kmeans.predict(arr)
+        print("Cluster:" + str(y[0]))
+        print("Keywords:" + str(keyword_dict[y[0]]))
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    
+    except rospy.ROSInterruptException:
+        pass
+    
