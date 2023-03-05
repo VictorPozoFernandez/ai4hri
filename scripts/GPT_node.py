9acc5cc7a84bbddcbcf1d27b110b7b1028f94898
victor <victorpozo125@gmail.com>
2023-03-05 22:19:24 +0100

    Improved gpt node

--- scripts/GPT_node.py
+++ scripts/GPT_node.py
@@ -13,15 +13,19 @@ You have the following camera models to choose from. You are not to use any othe
 
 - Nikon Coolpix S2800
 - Sony Alpha a6000
+- Sony Alpha a5000
 - Canon EOS 5D Mark III
 
-Here is an example scenario that illustrates how can you output your answer.:
+Here is an example scenario that illustrates how can you output your answer. The scenarios appear in cronological order and toghether represent a full conversation between the camera shopkeeper and the costumer:
 
-(SCENARIO 1) Customer: <Customer sentence> Shopkeeper: <Shopkeeper sentence>
-You: (SCENARIO 1) Camera Model - <all possible camera models> 
+Customer: <Customer sentence> Shopkeeper: <Shopkeeper sentence>
+Customer: <Customer sentence> Shopkeeper: <Shopkeeper sentence>
+You: Camera Model - <all possible camera models> 
 
-The scenarios appear in cronological order. Remember to use the previous scenarios to have more context about the camera model that is being discussed at each moment. 
+Remember to use the previous scenarios to have more context about the camera model that is being discussed at each moment. 
 In case of uncertainty about the camera model that is being discussed, output all the possible camera models instead.
+
+
 
 """
 
@@ -48,9 +52,10 @@ def callback(msg):
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
     messages=messages_history,
-    temperature=0
+    temperature=0.0
     )
 
+    print("--------------------------------------------")
     print(completion["choices"][0]["message"]["content"])
     #messages_history.append({"role": "assistant", "content": completion["choices"][0]["message"]["content"]})
     
