876a8bd0f07217635335f2b1cd0c6b55b4132092
victor <victorpozo125@gmail.com>
2023-03-07 11:56:14 +0100

    Added possibility of add characteristics of products that are closer to interaction

--- scripts/GPT_node.py
+++ scripts/GPT_node.py
@@ -3,8 +3,46 @@ from std_msgs.msg import String
 from ai4hri.msg import String_list
 import openai
 import os
+import mysql.connector
 
-#DEBUG = rospy.get_param('/whisper/DEBUG')
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=os.environ.get("MYSQL_PASSWRD"),
+  database="Camera_Store"
+)
+mycursorGPT = db.cursor(buffered=True)
+mycursorGPT2 = db.cursor(buffered=True)
+mycursorGPT3 = db.cursor(buffered=True)
+
+close_product_IDs = [1,2,3]
+characteristics_product_IDs = []
+
+for Product_ID in close_product_IDs:
+
+    characterstics_model = []
+    mycursorGPT.execute("SELECT table_name FROM information_schema.columns WHERE column_name = 'Product_ID'")
+
+    for table_name in mycursorGPT:
+
+        mycursorGPT2.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + table_name[0] + "'")
+        
+        for column_name in mycursorGPT2:
+        
+            if column_name[0] != 'Product_ID':
+
+                mycursorGPT3.execute("SELECT " + column_name[0]+ " FROM " + table_name[0] + " WHERE Product_ID = " + str(Product_ID))
+
+                column = []
+                for characteristic in mycursorGPT3:
+
+                    column.append(characteristic[0])
+                
+                characterstics_model.append((column_name[0], column))
+
+            
+    characteristics_product_IDs.append(characterstics_model)
+
 
 system_message = """Imagine you are helping me to identify the camera model that a shopkeeper is presenting to a customer. You are required to output the following answers:
 
@@ -29,8 +67,33 @@ Aproximate the characteristics of the camera that appear in <Shopkeeper sentence
 
 """
 
+message1 = """Imagine you are helping me to identify the camera model that a shopkeeper is presenting to a customer. You are required to output the following answers:
+
+- Camera model: All possible camera models that the shopkeeper is presenting.
+
+You have the following camera models to choose from. You are not to use any other hypothetical camera models:
+
+"""
+
+message2 = """ 
+
+Here is an example conversation that illustrates how can you output your answer. The interactions appear in cronological order, and together they represent a full conversation between the camera shopkeeper and the costumer:
+
+CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
+CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
+You: CAMERA MODEL - <all possible camera models> 
+
+Remember to use the previous interactions to have more context about all the possible camera models that the shopkeeper is presenting right now.
+Output only the camera model that the shopkeeper is presenting in the last interaction. 
+Aproximate the characteristics of the camera that appear in <Shopkeeper sentence> with the characteristics of the camera models to identify them.
+"""
+
+system_message2 = message1 + str(characteristics_product_IDs) + message2
+
+print(system_message2)
+
 messages_history=[
-    {"role": "system", "content": system_message}
+    {"role": "system", "content": system_message2}
     ]
 
 
