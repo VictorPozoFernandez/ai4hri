138d262ecceaeb68a3aa504bc13e39b22d000f82
victor <victorpozo125@gmail.com>
2023-03-08 09:35:08 +0100

    Created notebook to fine-tune a ML classification for detecting sentences of costumer or shopkeeper

--- scripts/GPT_node.py
+++ scripts/GPT_node.py
@@ -44,29 +44,6 @@ for Product_ID in close_product_IDs:
     characteristics_product_IDs.append(characterstics_model)
 
 
-system_message = """Imagine you are helping me to identify the camera model that a shopkeeper is presenting to a customer. You are required to output the following answers:
-
-- Camera model: All possible camera models that the shopkeeper is presenting.
-
-You have the following camera models to choose from. You are not to use any other hypothetical camera models:
-
-- Nikon Coolpix S2800. Characteristics (Point and Shoot/ color purple pink black silver red/ weight 120 grams/ 18 preset modes/ 12 glamour retouch effects/ price 68/ resolution 20.1 megapixels/ optical zoom 5x)
-- Sony Alpha a6000. Characteristics (Mirrorless/ color white silver black/ weight 470 grams/ 9 preset modes/ 13 artistic effect modes/ price 550/ resolution 24 megapixels/ ISO 3200)
-- Sony Alpha a5000. Characteristics (Mirrorless/ color black/ weight 400 grams/ 5 preset modes/ 13 artistic effect modes/ price 300/ resolution 24 megapixels/ ISO 2000)
-- Canon EOS 5D Mark III. Characteristics (DSLR/ color black/ weight 950/ price 2000/ resolution 22.3 megapixels/ ISO 6400/ silent shooting)
-
-Here is an example conversation that illustrates how can you output your answer. The interactions appear in cronological order, and together they represent a full conversation between the camera shopkeeper and the costumer:
-
-CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
-CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
-You: CAMERA MODEL - <all possible camera models> 
-
-Remember to use the previous interactions to have more context about all the possible camera models that the shopkeeper is presenting right now.
-Output only the camera model that the shopkeeper is presenting in the last interaction. 
-Aproximate the characteristics of the camera that appear in <Shopkeeper sentence> with the characteristics of the camera models to identify them.
-
-"""
-
 message1 = """Imagine you are helping me to identify the camera model that a shopkeeper is presenting to a customer. You are required to output the following answers:
 
 - Camera model: All possible camera models that the shopkeeper is presenting.
@@ -80,20 +57,19 @@ message2 = """
 Here is an example conversation that illustrates how can you output your answer. The interactions appear in cronological order, and together they represent a full conversation between the camera shopkeeper and the costumer:
 
 CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
-CUSTOMER: <Customer sentence> SHOPKEEPER: <Shopkeeper sentence>
-You: CAMERA MODEL - <all possible camera models> 
+You: CAMERA MODEL - <all possible camera model names> 
 
 Remember to use the previous interactions to have more context about all the possible camera models that the shopkeeper is presenting right now.
-Output only the camera model that the shopkeeper is presenting in the last interaction. 
+Output only the camera model name that the shopkeeper is presenting in the last interaction. 
 Aproximate the characteristics of the camera that appear in <Shopkeeper sentence> with the characteristics of the camera models to identify them.
 """
 
-system_message2 = message1 + str(characteristics_product_IDs) + message2
+system_message = message1 + str(characteristics_product_IDs) + message2
 
-print(system_message2)
+print(system_message)
 
 messages_history=[
-    {"role": "system", "content": system_message2}
+    {"role": "system", "content": system_message}
     ]
 
 
