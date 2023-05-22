bbd47c927cd47448291fee15905b0c3d6c63dd01
victor <victorpozo125@gmail.com>
2023-05-22 12:29:59 +0200

    Added simulator_node again to publish the conversation dataset from Malcolm

--- scripts/simulator_node.py
+++ scripts/simulator_node.py
@@ -1,9 +1,7 @@
 import rospy
-from ai4hri.msg import String_list
+from std_msgs.msg import String
 import pandas as pd
 
-DEBUG = rospy.get_param('/whisper/DEBUG')
-SIMULATOR = rospy.get_param('/whisper/SIMULATOR')
 
 
 def main():
@@ -11,7 +9,7 @@ def main():
     # Initialize the simulator ROS node and a publisher for the AI4HRI utterance_and_position topic
     rospy.init_node("simulator", anonymous=True)
     rospy.loginfo("Node simulator initialized...")
-    pub = rospy.Publisher('/ai4hri/utterance_and_position', String_list, queue_size= 10) 
+    pub = rospy.Publisher('/ai4hri/utterance', String, queue_size= 10) 
     rate = rospy.Rate(1)
 
     # Main loop that runs until the ROS node is shut down.
@@ -28,23 +26,21 @@ def main():
         num_interaction = input("Select interaction: ")
 
         # Read the simulated data from a CSV file and extract the relevant columns.
-        df = pd.read_csv("/home/victor/catkin_ws/src/ai4hri/simulated data.csv")
-        extracted_columns = df.iloc[:,[5, 14, 16, 25, 35]]
-        extracted_columns = extracted_columns.dropna()
+        df = pd.read_csv("/home/victor/catkin_ws/src/ai4hri/scripts/simplified_database.csv")
 
         # Filter the extracted columns by the selected interaction number.
-        interaction = extracted_columns[extracted_columns['TRIAL'] == int(num_interaction)]
-        interaction_no_trial = interaction.iloc[:,1:]
+        interaction = df[df['TRIAL'] == int(num_interaction)]
+        interaction_no_trial = interaction.iloc[:,1]
         
-        # Iterate through the rows of the selected interaction. Publish the message to the "/ai4hri/utterance_and_position" topic.
+        # Iterate through the rows of the selected interaction. Publish the message to the "/ai4hri/utterance" topic.
         num_rows = len(interaction_no_trial)
         for row in range(num_rows):
 
-            utterance = String_list()
-            utterance = interaction_no_trial.iloc[row,:].to_list()
+            utterance = String()
+            utterance = interaction_no_trial.iloc[row]
             pub.publish(utterance) 
             print("------------------------------------------")
-            print(interaction_no_trial.iloc[row,:].to_list())
+            print(interaction_no_trial.iloc[row])
             
             for _ in range(10):
                 rate.sleep()
@@ -53,8 +49,7 @@ def main():
 if __name__ == '__main__':
 
     try:
-        if SIMULATOR == True:
-            main()
+        main()
     
     except rospy.ROSInterruptException:
         pass
