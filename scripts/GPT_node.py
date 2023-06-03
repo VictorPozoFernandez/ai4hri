c0fdaebf844e7df4209add6d0375046e13a3644d
victor <victorpozo125@gmail.com>
2023-06-03 11:01:30 +0200

    Improved     detected_model_list= model_identification_gpt(msg, characteristics_products)

--- scripts/GPT_node.py
+++ scripts/GPT_node.py
@@ -28,20 +28,37 @@ def main():
 
 
 def callback(msg):
+
+    global previous_conversations
         
     # Set OpenAI API credentials
     openai.organization = os.environ.get("OPENAI_ORG_ID")
     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
     # Extract relevant modelS and topic from the message
-    detected_model_list = models_of_interest(msg)
+
+    topic = topic_extraction(msg)
+
+    characteristics_products = extraction_characteristics_products(products_of_interest, topic)
+    detected_model_list= model_identification_gpt(msg, characteristics_products)
+    print(detected_model_list)
+
+    try:
+        result = change_of_model_classification(previous_conversations, msg)
+        
+        if result["Detection"] == "['Different model']":
+            previous_conversations = ""
+
+    except Exception as e:
+        print(e)
+        previous_conversations = ""
+        pass
+
+    previous_conversations = previous_conversations + "Customer: " + str(msg.data[0]) + " Shopkeeper: " + str(msg.data[1])
 
     if "NULL" not in detected_model_list:
         #topic = topic_extraction(msg)
-        topic = topic_extraction_2(msg)
-        print(topic)
-
-
+        #extraction_list_products()
 
         # Initialize the publisher for extracted_info ROS topic
         pub = rospy.Publisher('/ai4hri/extracted_info', String_list, queue_size= 1, latch=True) 
@@ -65,105 +82,8 @@ def callback(msg):
             print("")
             print("ChatGPT1: Unrecognized model")  
 
-def models_of_interest(msg):
-
-    # Append the current interaction between the customer and shopkeeper to messages_history
-    messages_history.append({"role": "user", "content": "CURRENT INTERACTION: " + msg.data[0] + " ### " + msg.data[1]})
-
-    # Remove the oldest interaction in messages_history if it contains more that 10 interactions (used to define the long-term memory of Chat-GPT in a given context)
-    if len(messages_history) > 10:
-        messages_history.pop(1)
-
-    # Get the generated text from OpenAI's GPT-3.5-turbo model
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages=messages_history,
-    temperature=0.0)
-
-    if DEBUG == True:
-        print("")
-        print("ChatGPT1: " + str(completion["choices"][0]["message"]["content"]))
-    
-    detected_model_list = []
-    # Iterate through products_of_interest and check if they are mentioned in the generated text from ChatGPT
-    
-    for Product in products_of_interest:
-
-        if Product[1] in completion["choices"][0]["message"]["content"]:
-            detected_model_list.append(str(Product[0]))
-            detected_model_list.append(str(Product[1]))
-    
-    if len(detected_model_list) == 0:
-        detected_model_list.append(str("NULL"))
-
-    # Update messages_history with the previous interaction
-    messages_history.pop(-1)
-    messages_history.append({"role": "user", "content": "PREVIOUS INTERACTION: " + msg.data[0] + " ### " + msg.data[1]})
-    
-    return detected_model_list
-
 
 def topic_extraction(msg):
-
-    # Connect to the Camera_Store database. Initialize the cursor for querying the database.
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(parent_dir, "Camera_Store.db")
-    db = sqlite3.connect(db_path)
-    
-    mycursor = db.cursor()
-
-    # Get all table names in the database
-    mycursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = mycursor.fetchall()
-
-    # Iterate through tables. Get all column names. Create a list with all the the found column names in the database that can be considered as topic
-    utterances_to_compare = []
-    for table in tables:
-        table_name = table[0]
-        mycursor.execute(f"PRAGMA table_info({table_name});")
-        columns = mycursor.fetchall()
-        column_names = [column[1] for column in columns]
-
-        if 'Product_ID' in column_names:
-            utterances_to_compare = utterances_to_compare + column_names  
-
-    # Insert the current interaction between the customer and shopkeeper at the beginning of the list (it will be used to compare it with the previous column names)
-    interaction = msg.data[0] + msg.data[1]
-    utterances_to_compare.insert(0, interaction)
-
-    # Define the text embedding model and create the corresponding embeddings from the stored utterance and colum names
-    model = "text-embedding-ada-002"
-    res = openai.Embedding.create( input = utterances_to_compare, engine=model)
-    embedded_columns=[]
-    for vec in res["data"]:
-        embedded_columns.append(vec["embedding"])
-
-    # Separate the utterance embedding from the column embeddings
-    utterance = embedded_columns[0]
-    embedded_columns.pop(0)
-    utterances_to_compare.pop(0)
-
-    # Calculate cosine similarity scores between the utterance and column embedding
-    scores = []
-    for _, column_candidate in enumerate(embedded_columns):
-        score = cosine_similarity([utterance],[column_candidate])
-        scores.append(score)
-
-    # Select the top 3 scoring columns
-    selected_columns = []
-    best_scores = sorted(zip(scores, utterances_to_compare), reverse=True)[:4]
-
-    for score in best_scores:
-        selected_column= score[1].replace(" ", "_")
-        selected_columns.append(selected_column)
-
-    if DEBUG == True:
-        print("Topics: " + str(selected_columns))
-
-    return selected_columns
-
-
-def topic_extraction_2(msg):
 
     # Connect to the Camera_Store database. Initialize the cursor for querying the database.
     parent_dir = os.path.dirname(os.path.abspath(__file__))
@@ -255,41 +175,7 @@ def extract_json(s):
         return {"Detection": "null", "Model": "null", "Output" : "null",}
 
 
-def generating_system_instructions(products_of_interest):
-
-    message1 = """Imagine you are helping me to identify the camera model that a shopkeeper is presenting to a customer. You are required to output the following answers:
-
-    - Camera model: All possible camera models from the Authorized list that the shopkeeper is presenting in the current interaction. Remember to use the previous interactions to have more context about all the possible camera models that the shopkeeper is presenting in the CURRENT INTERACTION
-
-    Authorized list:
-    """
-
-    # Extract all the characteristics of the selected products from the SQL Database. 
-    # It will be used by ChatGPT to reason which product is the shopkeeper presenting, even if the name of the model is not explicitly said. 
-    characteristics_product_IDs = extraction_characteristics_products(products_of_interest)
-
-    message2 = """ Here is an example that illustrates how can you output your answer. The interactions appear in cronological order:
-
-    Previous interaction: <Customer sentence> ### <Shopkeeper sentence>;
-    Current interaction <Customer sentence> ### <Shopkeeper sentence>;
-    You: Camera model - <all possible authorized camera model names from the Authorized list> 
-
-    Output only the camera model name that the shopkeeper is presenting in the current interaction. Output only the model's name. Be concise.
-    """
-
-    # Put together previous string messages to obtain the final prompt that will be sent to ChatGPT.
-    system_message = message1 + str(characteristics_product_IDs) + message2
-    
-    if DEBUG == True:
-        print("")
-        print("Prompt sent to ChatGPT:")
-        print("")
-        print(system_message)
-
-    return system_message
-
-
-def extraction_characteristics_products(products_of_interest):
+def extraction_characteristics_products(products_of_interest, topics):
 
     # Connect to the Camera_Store database. Initialize the cursor for querying the database.
     parent_dir = os.path.dirname(os.path.abspath(__file__))
@@ -321,7 +207,7 @@ def extraction_characteristics_products(products_of_interest):
             # Check if 'Product_ID' exists in the table
             if 'Product_ID' in column_names:
                 for column_name in column_names:
-                    if column_name != 'Product_ID':
+                    if column_name in topics or column_name == "Model":
                         # Get the value of the current column for the current product
                         mycursorGPT3.execute(f"SELECT {column_name} FROM {table_name} WHERE Product_ID = ?", (Product[0],))
                         column = []
@@ -341,11 +227,138 @@ def extraction_characteristics_products(products_of_interest):
     return characteristics_product_IDs
 
 
+
+def model_identification_gpt(msg, characteristics_products):
+
+    # Set OpenAI API credentials
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    # Prepare prompt to send, using JSON format
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
+
+
+    system_prompt = """
+    You are a helpful assistant. Identify the camera model from the List that is currently being discussed 
+
+    Here there are some examples that illustrates how can you output your answer.
+
+    Customer: 'What is the price of this Sony Camera?' Shopkeeper: 'This is the Nikon Coolpix, and it costs 68 dollars';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Price', ['68'])], [('Model', ['Sony Alpha a6000']), ('Price', ['550'])], [('Model', ['Canon EOS 5D Mark III']), ('Price', ['2000'])]]
+    You: {"Detection": "['Nikon Coolpix S2800']"}
+
+    Customer: 'And what about this camera?' Shopkeeper: 'this model is only available in color black';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Color', ['black', 'pink', 'purple', 'red', 'silver'])], [('Model', ['Sony Alpha a6000']), ('Color', ['black', 'silver', 'white'])], [('Model', ['Canon EOS 5D Mark III']), ('Color', ['black'])]]
+    You: {"Detection": "['Canon EOS 5D Mark III']"}
+
+    Customer: 'Has this camera 18 preset modes?' Shopkeeper: 'No, it only has 9 preset modes';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Camera_features', ['12 Glamour retouch effects', '18 preset modes', 'Optical Zoom 5x'])], [('Model', ['Sony Alpha a6000']), ('Camera_features', ['13 artistic effect modes', '9 preset modes'])], [('Model', ['Canon EOS 5D Mark III']), ('Camera_features', ['Silent Shooting'])]]
+    You: {"Detection": "['Sony Alpha a6000']"}
+
+    Customer: 'And the weight of this one?' Shopkeeper: 'This camera weights 95 grams';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Weight', ['120 grams'])], [('Model', ['Sony Alpha a6000']), ('Weight', ['470 grams'])], [('Model', ['Canon EOS 5D Mark III']), ('Weight', ['950 grams'])]]
+    You: {"Detection": "['NULL']"}
+
+    Customer: 'How much does it cost this camera?' Shopkeeper: 'This one costs 200 dollars';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Price', ['68'])], [('Model', ['Sony Alpha a6000']), ('Price', ['550'])], [('Model', ['Canon EOS 5D Mark III']), ('Price', ['2000'])]]
+    You: {"Detection": "['NULL']"}
+
+    Customer: 'What about its resolution?' Shopkeeper: 'It has more than 21 megapixels';
+    List: [[('Model', ['Nikon Coolpix S2800']), ('Resolution', ['20.1 megapixels'])], [('Model', ['Sony Alpha a6000']), ('Resolution', ['24.0 megapixels'])], [('Model', ['Canon EOS 5D Mark III']), ('Resolution', ['22.3 megapixels'])]]
+    You: {"Detection": "['Sony Alpha a6000', 'Canon EOS 5D Mark III']"}
+
+    Use the data from the List to identify the camera model. If no camera is detected, output {"Detection": "['NULL']"} 
+    Output the answer only in JSON format.
+    """
+
+    user_template = """
+    Customer: {customer} Shopkeeper: {shopkeeper}
+    List: {characteristics_products}
+    """
+
+    user_prompt_template = PromptTemplate(input_variables=["customer", "shopkeeper", "characteristics_products"], template=user_template)
+    user_prompt = user_prompt_template.format(customer = msg.data[0], shopkeeper = msg.data[1], characteristics_products = characteristics_products)
+
+    prompt_history = [
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=user_prompt)
+    ]
+
+    result = chat(prompt_history)
+    data = extract_json(result.content)
+
+    detected_model_list = []
+    # Iterate through products_of_interest and check if they are mentioned in the generated text from ChatGPT
+    
+    for Product in products_of_interest:
+
+        if Product[1] in data["Detection"]:
+            detected_model_list.append(str(Product[0]))
+            detected_model_list.append(str(Product[1]))
+    
+    if len(detected_model_list) == 0:
+        detected_model_list.append(str("NULL"))
+
+    return detected_model_list
+
+
+def change_of_model_classification(previous_conversations, msg):
+
+    # Set OpenAI API credentials
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    # Prepare prompt to send, using JSON format
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
+
+
+    system_prompt = """
+    You are a helpful assistant that identifies if the Customer or the Shopkeeper start talking about a different model than the one they where talking in the previous interaction.
+
+    Here there are some examples that illustrates how can you output your answer. The interactions appear in cronological order:
+
+    Previous interactions -> Customer: 'What is the price of this Sony Camera?' Shopkeeper: 'This is the Sony Alpha, and it costs 550 dollars';
+    Current interaction -> Customer: 'I see, and how much does it weight?' Shopkeeper: 'only 120 grams';
+    You: {"Reasoning": "The Customer is asking about the weight of the camera that was presented in the previous interaction. They are keep talking about the same model as the Previous interaction", "Detection": "['Same model']"}
+
+    Previous interactions -> Customer: 'I would like to buy a cheap camera' Shopkeeper: 'In this case I recommend you the Nikon Coolpix';
+    Current interaction -> Customer: 'Do you have any other camera?' Shopkeeper: 'Yes, this is the Cannon EOS 5D, one of the best cameras of the market';
+    You: {"Reasoning": "The Customer is asking about other cameras and the Shopkeeper presents the Cannon EOS 5D model. They are keep talking about a different model than in the Previous interaction", "Detection": "['Different model']"}
+
+    Previous interactions -> Customer: 'How much does it cost?' Shopkeeper: '2000 dollars';
+    Current interaction -> Customer: 'And the price of the first camera that you showed me?' Shopkeeper: '68 dollars';
+    You: {"Reasoning": "The Customer is asking about another camera that was presented previously bt the Shopkeeper. They are keep talking about a different model than in the Previous interaction", "Detection": "['Different model']"}
+
+    Previous interactions -> Customer: '' Shopkeeper: 'Good morning';
+    Current interaction -> Customer: 'What's the price of this camera?' Shopkeeper: 'This is the Cannon EOS 5D, it costs 2000 dollars';
+    You: {"Reasoning": "This is the first interaction, so there is no previous model to compare with.", "Detection": "['First interaction']"}
+
+
+    Output only with the labels ['Same model'], ['Different model'] or ['First interaction']
+    Output the answer only in JSON format.
+    """
+
+    user_template = """
+    Previous interactions -> {previous_conversations}
+    Current interaction -> Customer: {customer} Shopkeeper: {shopkeeper}
+    """
+
+    user_prompt_template = PromptTemplate(input_variables=["previous_conversations", "customer", "shopkeeper"], template=user_template)
+    user_prompt = user_prompt_template.format(previous_conversations = previous_conversations, customer = msg.data[0], shopkeeper = msg.data[1])
+
+    prompt_history = [
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=user_prompt)
+    ]
+
+    result = chat(prompt_history)
+    data = extract_json(result.content)
+    print(data)
+
+    return data
+
+
 if __name__ == '__main__':
 
     try:
-        system_message = generating_system_instructions(products_of_interest)
-        messages_history=[{"role": "system", "content": system_message}]
         main()
     
     except rospy.ROSInterruptException:
