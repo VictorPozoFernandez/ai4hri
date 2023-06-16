763b565a87118aa413d6564f1e389dcdacc0719d
victor <victorpozo125@gmail.com>
2023-06-16 20:36:56 +0200

    changed lists for JSON in judge_gpt

--- scripts/MySQL_node.py
+++ scripts/MySQL_node.py
@@ -69,9 +69,7 @@ def search_callback(msg):
         msg.data.pop(0)
     
     ground_truth = extract_ground_truth(models_interest, topics_interest)
-    result = judge_gpt(shopkeeper_sentence, ground_truth)
-    
-    substrings = extract_substrings(result)
+    detections = judge_gpt(shopkeeper_sentence, ground_truth)
     
     if last_insert_rowid == "":
 
@@ -89,53 +87,33 @@ def search_callback(msg):
                     last_insert_rowid = result[0] + 1
                 else:
                     last_insert_rowid = 1
-                    
-    
-    for substring in substrings:
-        #print("")
-        #print(substring)
 
-        try:
-            substring = ast.literal_eval(substring)
-            feature = get_left_substring(substring[3])
+    try:
+        
+        for detection in detections:
 
-            if (substring[1] != 'NOT MENTIONED') and (feature in topics_interest):
+            if (detection["Detection"] != 'NOT MENTIONED') and (detection["Feature"] in topics_interest):
                 
                 print("")
-                if substring[1] == "SHOPKEEPER IS RIGHT":
-                    print("\033[92m" + substring[1] + "\033[0m")
-                elif substring[1] == "SHOPKEEPER IS MISTAKEN":
-                    print("\033[91m" + substring[1] + "\033[0m")
+                if detection["Detection"] == "SHOPKEEPER IS RIGHT":
+                    print("\033[92m" + detection["Detection"] + "\033[0m")
+                elif detection["Detection"] == "SHOPKEEPER IS MISTAKEN":
+                    print("\033[91m" + detection["Detection"] + "\033[0m")
                 else:
-                    print("\033[93m" + substring[1] + "\033[0m")
+                    print("\033[93m" + detection["Detection"] + "\033[0m")
 
                 #print("Product: " + substring[2])
                 #print("Feature: " + feature)
-                print("Reason: " + substring[0])
+                print("Reason: " + detection["Explanation"])
 
                 query = "INSERT INTO Detections (Interaction_ID, Utterance, Class, Feature, Reason) VALUES (?, ?, ?, ?, ?);"
-                params = (last_insert_rowid ,customer_sentence + " // " + shopkeeper_sentence, substring[1], feature, substring[0])
+                params = (last_insert_rowid ,customer_sentence + " // " + shopkeeper_sentence, detection["Detection"], detection["Feature"], detection["Explanation"])
                 mycursor7.execute(query, params)
 
                 db2.commit()
-        
-        except Exception as e:
-            print(e)
-            
 
-
-def extract_substrings(text):
-    pattern = r'##\[(.*?)\]##'
-    substrings = re.findall(pattern, text)
-    return substrings
-
-
-def get_left_substring(text):
-    if ':' in text:
-        left_substring = text.split(':', 1)[0].strip()
-        return left_substring
-    else:
-        return text
+    except Exception as e:
+        print(e)
     
 
 def extract_ground_truth(models_interest, topics_interest):
@@ -176,24 +154,76 @@ def judge_gpt(shopkeeper_sentence, ground_truth):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo-0613", temperature=0, openai_api_key=openai_api_key)
 
     system_prompt = """
-    You are a helpful assistant that identifies if a shopkeeper is mistaken when presenting the characteristics of a camera model. Your task is to analyze the information given in Ground Truth and output the relevant lists based on the characteristics mentioned by the shopkeeper:
+    You are a helpful assistant that identifies if a shopkeeper is mistaken when presenting the characteristics of a camera model. Your task is to analyze the information given in Ground Truth and output the relevant JSON objects based on the characteristics mentioned by the shopkeeper:
 
-    1. If the shopkeeper is right about a characteristic from Ground Truth, output: ##['<Explain why the Shopkeeper shopkeeper is right>', 'SHOPKEEPER IS RIGHT', '<presented camera model>', '<feature from Ground Truth>']##
+    1. If the shopkeeper is right about a characteristic from Ground Truth, output:
+    [
+        {
+            "Explanation": "<Explain why the Shopkeeper shopkeeper is right>",
+            "Detection": "SHOPKEEPER IS RIGHT",
+            "Model": "<presented camera model>",
+            "Feature": "<feature from Ground Truth>"
+        }
+    ]
+
     2. If the shopkeeper is mistaken about a characteristic from Ground Truth, output: ##['<Explain why the Shopkeeper is mistaken>', 'SHOPKEEPER IS MISTAKEN', '<presented camera model>', '<feature from Ground Truth>']##
+    [
+        {
+            "Explanation": "<Explain why the Shopkeeper shopkeeper is mistaken>",
+            "Detection": "SHOPKEEPER IS MISTAKEN",
+            "Model": "<presented camera model>",
+            "Feature": "<feature from Ground Truth>"
+        }
+    ]
+
     3. If the shopkeeper does not mention a characteristic from Ground Truth, output: ##['null', 'NOT MENTIONED', '<presented camera model>', '<feature from Ground Truth>']##
+    [
+        {
+            "Explanation": "null",
+            "Detection": "NOT MENTIONED",
+            "Model": "<presented camera model>",
+            "Feature": "<feature from Ground Truth>"
+        }
+    ]
+
     4. If the shopkeeper is not able to answer, or asks for help, output: ##['<Explain why the shopkeeper is not able to answer>', 'SHOPKEEPER DOESNT KNOW', '<presented camera model>', '<feature from Ground Truth>']##
+    [
+        {
+            "Explanation": "<Explain why the Shopkeeper shopkeeper is not able to answer",
+            "Detection": "SHOPKEEPER DOESNT KNOW",
+            "Model": "<presented camera model>",
+            "Feature": "<feature from Ground Truth>"
+        }
+    ]
     
-    When the shopkeeper presents multiple characteristics, output a separate list for each characteristic mentioned by the shopkeeper. 
+    When the shopkeeper presents multiple characteristics, output an JSON object for each characteristic mentioned by the shopkeeper. 
     Use the information given in Ground Truth to help you reason. Here's an example of how to format your answer:
 
     Shopkeeper utterance: <The Sony camera is a Mirrorless camera that has a resolution of 20 megapixels>
     Ground Truth: ["Sony Alpha a6000: ['Model: Sony Alpha a6000', 'Price: <550>', 'Type_of_camera: <Mirrorless>', 'Resolution: <18.0 megapixels>']"]
-    ##['The Sony Alpha a6000 is indeed a Mirrorless camera', 'SHOPKEEPER IS RIGHT', 'Sony Alpha a6000', 'Type_of_camera']##
-    ##['The Sony Alpha a6000 has a resolution of 18 megapixels, not 20', 'SHOPKEEPER IS MISTAKEN', 'Sony Alpha a6000', 'Resolution']##
-    ##['The Shopkeeper doesnt mention any price ', 'NOT MENTIONED', 'Sony Alpha a6000', 'Price']##
-    
+    You:[
+            {
+                "Explanation": "The Sony Alpha a6000 is indeed a Mirrorless camera",
+                "Detection": "SHOPKEEPER IS RIGHT",
+                "Model": "Sony Alpha a6000",
+                "Feature": "Type_of_camera"
+            },
+            {
+                "Explanation": "The Sony Alpha a6000 has a resolution of 18 megapixels, not 20",
+                "Detection": "SHOPKEEPER IS MISTAKEN",
+                "Model": "Sony Alpha a6000",
+                "Feature": "Resolution"
+            },
+            {
+                "Explanation": "The Shopkeeper doesnt mention any price",
+                "Detection": "NOT MENTIONED",
+                "Model": "Sony Alpha a6000",
+                "Feature": "Price"
+            }
+        ]
+
     Keep your response concise. 
-    Always include the ## characters at the beginning and the end of each list.
+    Output the answer only in JSON format.
     """
 
     user_template = """
@@ -213,13 +243,27 @@ def judge_gpt(shopkeeper_sentence, ground_truth):
         print("")
         print(user_prompt)
         result=input("judge_gpt:")
+        data = extract_json(result)
 
     else:
         result = chat(prompt_history)
-        #print(result.content)
-        result=result.content
+        data = extract_json(result.content)
 
-    return result
+    return data
+
+
+def extract_json(s):
+    json_match = re.search(r'\[.*\]', s, re.DOTALL)
+    if json_match:
+        try:
+            data = json.loads(json_match.group())
+            return data
+        except json.JSONDecodeError:
+            print("Invalid JSON")
+            return None
+    else:
+        print("No JSON found in the string")
+        return {"Detection": "null", "Model": "null", "Output" : "null",}
 
 
 if __name__ == '__main__':
