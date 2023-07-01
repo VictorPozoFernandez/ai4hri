d4329a19e7e4bc994a567d22aa4e0d221bf2dd64
victor <victorpozo125@gmail.com>
2023-07-01 19:48:46 +0200

    added multiple languages ai4hri

--- scripts/record_node.py
+++ scripts/record_node.py
@@ -2,63 +2,103 @@ import rospy
 from audio_common_msgs.msg import AudioData
 from std_msgs.msg import String
 import speech_recognition as sr
+from dynamic_reconfigure.server import Server
+from ai4hri.cfg import audiorecorder_ai4hriConfig
 
 global audio_control
-audio_control = "RESUME"
+audio_control = "STOP"
 
 def callback(msg):
     global audio_control
     audio_control = msg.data
 
-def record_audio(pub, energy, pause):
+def callback2(config, level):
+
+    rospy.loginfo("""Reconfigure Request: {int_energy} """.format(**config))
+    
+    global energy
+    energy=config.int_energy
+
+    return config
+
+def record_audio(pub):
 
     global audio_control
+    global energy
 
     # Initialize the recognizer and set energy and pause thresholds
     r = sr.Recognizer()
     r.energy_threshold = energy
-    r.pause_threshold = pause
+    r.pause_threshold = 0.5
 
-    # Open the microphone with the specified sample rate and device index
-    counter = 0
-    found_micro = False
-    while (counter < 15) and (found_micro == False) :
-        try:      
-            with sr.Microphone(sample_rate=16000,  device_index=counter) as source:
+    try:      
 
-                # Clear the console
-                for x in range(4):
-                    print("")
-                rospy.loginfo("Node record_node initialized. Listening...")
+         # Print available microphones PC
 
+        rospy.sleep(2)
+        print("")
+        for index, name in enumerate(sr.Microphone.list_microphone_names()): print(f'{index}, {name}')
+        print('')
+        counter = input('Choose a microphone device: ')
+
+        with sr.Microphone(sample_rate=16000,  device_index=int(counter)) as source:
+
+            print("")
+            mode = input('Choose a recording mode (1.Automatic, 2.Manual): ')
+            
+            # Clear the console
+            for x in range(4):
+                print("")
+            rospy.loginfo("Node record_node initialized. Listening...")
+
+            if mode == "1":
                 # Record audio while the ROS node is running
                 while not rospy.is_shutdown():
                     audio = r.listen(source)
                     wav_data = audio.get_wav_data()
+                    pub.publish(wav_data)
+            
+            if mode == "2":
+                complete_audio=None
 
-                    if (audio_control == "RESUME"):
+                # Record audio while the ROS node is running
+                while not rospy.is_shutdown():
+                    
+                    audio = r.record(source, duration=1)  # Record for a small duration
+
+                    if audio_control == "RESUME":
+
+                            # Concatenate audio chunks if any speech is detected
+                        if complete_audio == None:
+                            complete_audio = sr.AudioData(audio.frame_data, audio.sample_rate, audio.sample_width)
+                        else:
+                            complete_audio = sr.AudioData(complete_audio.frame_data + audio.frame_data, audio.sample_rate, audio.sample_width)
+                    
+                    elif (audio_control == "STOP") and (complete_audio != None):              
+                        
+                        complete_audio = sr.AudioData(complete_audio.frame_data + audio.frame_data, audio.sample_rate, audio.sample_width)
+                        wav_data = complete_audio.get_wav_data()
                         pub.publish(wav_data)
+                        complete_audio = None
+                        
+                    else:
+                        previous_audio = audio
+                        complete_audio = None
 
-                    found_micro = True
-        except:
-            counter += 1
-            if counter >= 15:
-                print("Microphone not found")
-                break
+    
+    except Exception as e:
+        print(e)
+        print("Microphone not found")
+                
 
 def main():
     # Initialize the whisper ROS node and a publisher for the AI4HRI utterance topic
     rospy.init_node("audio_recorder", anonymous=True)
     pub = rospy.Publisher('/ai4hri/audio_data', AudioData, queue_size= 1)
     rospy.Subscriber("/ai4hri/audio_control", String, callback)
+    srv = Server(audiorecorder_ai4hriConfig, callback2)
 
-    energy = 25000
-    pause = 0.5
-
-    # Print available microhpones PC
-    for index, name in enumerate(sr.Microphone.list_microphone_names()): print(f'{index}, {name}')
-
-    record_audio(pub, energy, pause)
+    record_audio(pub)
 
 
 if __name__ == '__main__':
