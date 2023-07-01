89ae72b7fb50630b34b107931b32941d5bedfd52
victor <victorpozo125@gmail.com>
2023-07-01 18:11:35 +0200

    multiple languages

--- scripts/transcribe_node.py
+++ scripts/transcribe_node.py
@@ -5,6 +5,9 @@ import openai
 import os
 import queue
 import tempfile
+from dynamic_reconfigure.server import Server
+from ai4hri.cfg import audiotranscriber_ai4hriConfig
+
 
 openai.organization = os.environ.get("OPENAI_ORG_ID")
 openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -19,12 +22,14 @@ def main():
     #The system never listens to the first utterance. This snippet code publishes a first utterance that will be ignored.
     pub = rospy.Publisher('/ai4hri/utterance', String, queue_size= 1)
     pub.publish("------")
+    srv = Server(audiotranscriber_ai4hriConfig, callback2)
 
     rospy.sleep(1)
     rospy.spin()
 
 
 def callback(msg):
+    global language
     audio_data = msg.data
 
     # Save the audio_data as a temporary file
@@ -32,7 +37,6 @@ def callback(msg):
         temp_file.write(audio_data)
         temp_file_name = temp_file.name
 
-    language = os.environ.get("LANGUAGE_WHISPER")
     if language == None:
         language = "en" #Change language (english = "en", spanish = "es", french = "fr", german = "de", italian = "it", japanese = "ja")  
 
@@ -49,6 +53,15 @@ def callback(msg):
         pub = rospy.Publisher('/ai4hri/utterance', String, queue_size= 1)
         pub.publish(utterance)
 
+
+def callback2(config, level):
+
+    rospy.loginfo("""Reconfigure Request: {language_str_param} """.format(**config))
+    
+    global language
+    language=config.language_str_param
+
+    return config
 
 if __name__ == '__main__':
     try:
